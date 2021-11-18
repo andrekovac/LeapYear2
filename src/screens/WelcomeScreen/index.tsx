853dc62ad8072200845716fad66117758d315d65
André Kovac <me@andrekovac.com>
2021-11-19 00:47:38 +0100

    add spring animation

--- src/screens/WelcomeScreen/index.tsx
+++ src/screens/WelcomeScreen/index.tsx
@@ -1,19 +1,59 @@
-import React, { VFC } from "react";
+import React, { useEffect, useRef, VFC } from "react";
+import { Animated, Easing } from "react-native";
+import styled from "styled-components/native";
+
 import StartButton from "../../components/StartButton";
-import { StyledText } from "../../components/StartButton/styles";
 
 interface Props {
   onPress: () => void;
+  hasPressedButton: boolean;
 }
-const WelcomeScreen: VFC<Props> = ({ onPress }) => {
+const WelcomeScreen: VFC<Props> = ({ onPress, hasPressedButton }) => {
+  const introAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    bounceIn();
+    return () => {
+      introAnim.setValue(0);
+    };
+  }, [hasPressedButton]);
+
+  const bounceIn = () => {
+    Animated.timing(introAnim, {
+      toValue: 1,
+      duration: 1000,
+      easing: Easing.bounce,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <>
-      <StyledText size={80}>{"Leap Year"}</StyledText>
-      <StartButton onPress={onPress}>
-        {"Start"}
-      </StartButton>
+      <AnimatedText
+        style={{
+          transform: [
+            {
+              scale: introAnim.interpolate({
+                // Animated.Value goes from 0 to 1
+                inputRange: [0, 1],
+                // scale value changes from 1 to 2
+                outputRange: [1, 2],
+              }),
+            },
+          ],
+        }}
+      >
+        LeapYear
+      </AnimatedText>
+      <StartButton onPress={onPress}>{"Start"}</StartButton>
     </>
   );
 };
+
+const AnimatedText = styled(Animated.Text)<{ size?: number }>`
+  font-size: ${(props) => (props.size ? `${props.size}px` : "30px")};
+  font-weight: 100;
+  text-align: center;
+`;
 
 export default WelcomeScreen;
