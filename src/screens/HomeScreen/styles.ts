79ba322763a0b91e5e2f7d67235316f85436498e
André Kovac <me@andrekovac.com>
2021-11-19 07:58:49 +0100

    Add fade-in animation on HomeScreen

---
+++ src/screens/HomeScreen/styles.ts
@@ -0,0 +1,26 @@
+import { Animated } from "react-native";
+import styled from "styled-components/native";
+
+export const KeyboardContainer = styled.KeyboardAvoidingView`
+  flex: 1;
+`;
+
+export const AnimatedWrapper = styled(Animated.View)`
+  flex: 1;
+  justify-content: space-around;
+`;
+
+export const TextInputWrapper = styled.View`
+  min-width: 100%;
+`;
+
+export const YearTextInput = styled.TextInput`
+  height: 60px;
+  margin: 12px;
+  padding: 12px;
+
+  border: 3px solid black;
+  border-radius: 5px;
+
+  font-size: 20px;
+`;
