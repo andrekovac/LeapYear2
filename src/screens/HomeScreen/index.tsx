e7952e97d508102d7b927cbd4c334879f5876045
André Kovac <me@andrekovac.com>
2021-11-19 07:25:06 +0100

    add text input and keyboard avoiding view

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,16 +1,54 @@
-import React, { VFC } from "react";
+import React, { useState, VFC } from "react";
+import {
+  Keyboard,
+  Platform,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import styled from "styled-components/native";
+
 import ReturnButton from "../../components/ReturnButton";
 import { StyledText } from "../../components/StartButton/styles";
+import leapYearText from "../../util/leapYear";
+import {
+  AnimatedWrapper,
+  KeyboardContainer,
+  TextInputWrapper,
+  YearTextInput,
+} from "./styles";
 
 interface Props {
   onPress: () => void;
 }
-const HomeScreen: VFC<Props> = ({ onPress }) => (
-  <>
-    <StyledText>{"You entered the App"}</StyledText>
-    <ReturnButton text="Return" onPress={onPress} />
-  </>
-);
+const HomeScreen: VFC<Props> = ({ onPress }) => {
+  const [year, setYear] = useState<string>("2021");
+
+  return (
+    <KeyboardContainer behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Wrapper>
+          <StyledText size={70}>Leap Year</StyledText>
+          <View>
+            <StyledText size={24}>{leapYearText(year)}</StyledText>
+            <TextInputWrapper>
+              <YearTextInput
+                keyboardType={"numeric"}
+                maxLength={4}
+                onChangeText={setYear}
+                placeholder={"2021"}
+              />
+            </TextInputWrapper>
+          </View>
+          <ReturnButton text="Return" onPress={onPress} />
+        </Wrapper>
+      </TouchableWithoutFeedback>
+    </KeyboardContainer>
+  );
+};
+
+export const Wrapper = styled.View`
+  flex: 1;
+  justify-content: space-around;
+`;
 
 export default HomeScreen;
-
